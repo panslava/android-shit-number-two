18f1db44bb4fcd4028a342e8c2715bce66b805e3
Viacheslav Panasovets <pansslava@gmail.com>
2020-09-17 02:30:06 +0300

    remove broken drawables

--- app/src/main/java/com/example/podcasts/UploadActivity.kt
+++ app/src/main/java/com/example/podcasts/UploadActivity.kt
@@ -1,19 +1,25 @@
 package com.example.podcasts
 
 import android.Manifest
+import android.R.attr
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Outline
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
-import android.view.ViewOutlineProvider
-import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_upload.*
+import java.io.File
+import java.io.FileNotFoundException
+
 
 class UploadActivity : AppCompatActivity() {
     companion object {
@@ -30,7 +36,7 @@ class UploadActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
 
-        upload_image_preview.clipToOutline = true
+//        upload_image_preview.clipToOutline = true
         upload_image_preview.visibility = View.INVISIBLE
 
         val imageUploadOverlay = findViewById<ConstraintLayout>(R.id.upload_image_overlay)
@@ -74,12 +80,50 @@ class UploadActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == IMAGE_PICK_CODE && resultCode == Activity.RESULT_OK && data != null) {
-            upload_image_preview.setImageURI(data.data)
+        if (requestCode == IMAGE_PICK_CODE && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
+            val selectedImageUri: Uri = data.data!!
+            println(data.data)
+            println(selectedImageUri)
+            val selectedImagePath = getPath(selectedImageUri)
+            print(selectedImagePath)
+            val mImagePath = selectedImagePath
+            val photo = getPreview(selectedImagePath)
+            upload_image_preview.setImageBitmap(photo)
+//            try {
+//                Glide.with(this).load(data.data).into(upload_image_preview)
+//                upload_image_preview.setImageURI(data.data)
+//            }
+//            catch (ex: FileNotFoundException) {
+//                println("exception caught")
+//            }
             upload_image_icon.visibility = View.INVISIBLE
             upload_image_preview.visibility = View.VISIBLE
             imageLoaded = true
         }
+    }
+
+    fun getPath(uri: Uri?): String? {
+        val projection =
+            arrayOf(MediaStore.Images.Media.DATA)
+        val cursor: Cursor = managedQuery(uri, projection, null, null, null)
+        val column_index: Int = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        cursor.moveToFirst()
+        return cursor.getString(column_index)
+    }
+
+    fun getPreview(fileName: String?): Bitmap? {
+        val image = File(fileName)
+        val bounds = BitmapFactory.Options()
+        bounds.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(image.getPath(), bounds)
+        if (bounds.outWidth == -1 || bounds.outHeight == -1) {
+            return null
+        }
+        val originalSize =
+            if (bounds.outHeight > bounds.outWidth) bounds.outHeight else bounds.outWidth
+        val opts = BitmapFactory.Options()
+        opts.inSampleSize = originalSize / 64
+        return BitmapFactory.decodeFile(image.getPath(), opts)
     }
 
     override fun onRequestPermissionsResult(
