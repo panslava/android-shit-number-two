195886273b59926c9fd935f3d576361f8caca1c7
Viacheslav Panasovets <pansslava@gmail.com>
2020-09-17 05:55:02 +0300

    finished upload

--- app/src/main/java/com/example/podcasts/UploadActivity.kt
+++ app/src/main/java/com/example/podcasts/UploadActivity.kt
@@ -1,24 +1,23 @@
 package com.example.podcasts
 
 import android.Manifest
-import android.R.attr
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.Cursor
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.media.MediaMetadataRetriever
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.OpenableColumns
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_upload.*
-import java.io.File
-import java.io.FileNotFoundException
 
 
 class UploadActivity : AppCompatActivity() {
@@ -30,13 +29,15 @@ class UploadActivity : AppCompatActivity() {
 
         //Permission code
         private val PERMISSION_CODE = 1001;
+
+        private val AUDIO_PICK_CODE = 1002;
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
 
-//        upload_image_preview.clipToOutline = true
+        upload_image_preview.clipToOutline = true
         upload_image_preview.visibility = View.INVISIBLE
 
         val imageUploadOverlay = findViewById<ConstraintLayout>(R.id.upload_image_overlay)
@@ -56,8 +57,6 @@ class UploadActivity : AppCompatActivity() {
             } else {
                 removeImage()
             }
-
-
         }
     }
 
@@ -78,53 +77,114 @@ class UploadActivity : AppCompatActivity() {
         )
     }
 
+    fun onAudioUploadButton(view: View) {
+        val intentUpload = Intent()
+        intentUpload.type = "audio/*"
+        intentUpload.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intentUpload, AUDIO_PICK_CODE)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == IMAGE_PICK_CODE && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
-            val selectedImageUri: Uri = data.data!!
-            println(data.data)
-            println(selectedImageUri)
-            val selectedImagePath = getPath(selectedImageUri)
-            print(selectedImagePath)
-            val mImagePath = selectedImagePath
-            val photo = getPreview(selectedImagePath)
-            upload_image_preview.setImageBitmap(photo)
-//            try {
-//                Glide.with(this).load(data.data).into(upload_image_preview)
-//                upload_image_preview.setImageURI(data.data)
-//            }
-//            catch (ex: FileNotFoundException) {
-//                println("exception caught")
-//            }
+            try {
+                val imageUri = data.data!!;
+                val imageStream = contentResolver.openInputStream(imageUri);
+                val selectedImage = BitmapFactory.decodeStream(imageStream);
+
+                upload_image_preview.setImageBitmap(selectedImage)
+            } catch (ex: Exception) {
+                println("In exception")
+                Glide.with(this).load(data.data).into(upload_image_preview)
+
+                println("exception caught")
+            }
             upload_image_icon.visibility = View.INVISIBLE
             upload_image_preview.visibility = View.VISIBLE
             imageLoaded = true
         }
-    }
+        if (requestCode == AUDIO_PICK_CODE && resultCode == Activity.RESULT_OK) {
+            val selectedImage = data?.data
+            val filePath = selectedImage?.let { getFileName(it) }
 
-    fun getPath(uri: Uri?): String? {
-        val projection =
-            arrayOf(MediaStore.Images.Media.DATA)
-        val cursor: Cursor = managedQuery(uri, projection, null, null, null)
-        val column_index: Int = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-        cursor.moveToFirst()
-        return cursor.getString(column_index)
-    }
+            println("filepath")
+            println(filePath)
 
-    fun getPreview(fileName: String?): Bitmap? {
-        val image = File(fileName)
-        val bounds = BitmapFactory.Options()
-        bounds.inJustDecodeBounds = true
-        BitmapFactory.decodeFile(image.getPath(), bounds)
-        if (bounds.outWidth == -1 || bounds.outHeight == -1) {
-            return null
+            val mmr = MediaMetadataRetriever()
+            mmr.setDataSource(this, selectedImage)
+            val durationStr =
+                mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+
+            val millSecond = durationStr!!.toLong()
+            val text = formatMilliSeccond(millSecond)
+
+            upload_length.text = text
+            upload_filename.text = filePath
+            upload_center_not_uploaded.visibility = View.INVISIBLE
+            upload_center_uploaded.visibility = View.VISIBLE
+
+            upload_next_button.background = ContextCompat.getDrawable(this, R.drawable.blue_button)
         }
-        val originalSize =
-            if (bounds.outHeight > bounds.outWidth) bounds.outHeight else bounds.outWidth
-        val opts = BitmapFactory.Options()
-        opts.inSampleSize = originalSize / 64
-        return BitmapFactory.decodeFile(image.getPath(), opts)
     }
+
+    private fun getFileName(uri: Uri): String? {
+        var result: String? = null
+        if (uri.scheme.equals("content")) {
+            val cursor =
+                contentResolver.query(uri, null, null, null, null)
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                }
+            } finally {
+                cursor!!.close()
+            }
+        }
+        if (result == null) {
+            result = uri.getPath()
+            val cut = result?.lastIndexOf('/')
+            if (cut != -1) {
+                if (result != null) {
+                    if (cut != null) {
+                        result = result.substring(cut + 1)
+                    }
+                }
+            }
+        }
+        return result
+    }
+
+    fun formatMilliSeccond(milliseconds: Long): String? {
+        var finalTimerString = ""
+        var secondsString = ""
+
+        // Convert total duration into time
+        val hours = (milliseconds / (1000 * 60 * 60)).toInt()
+        val minutes = (milliseconds % (1000 * 60 * 60)).toInt() / (1000 * 60)
+        val seconds = (milliseconds % (1000 * 60 * 60) % (1000 * 60) / 1000).toInt()
+
+        // Add hours if there
+        if (hours > 0) {
+            finalTimerString = "$hours:"
+        }
+
+        // Prepending 0 to seconds if it is one digit
+        secondsString = if (seconds < 10) {
+            "0$seconds"
+        } else {
+            "" + seconds
+        }
+        finalTimerString = "$finalTimerString$minutes:$secondsString"
+
+        //      return  String.format("%02d Min, %02d Sec",
+        //                TimeUnit.MILLISECONDS.toMinutes(milliseconds),
+        //                TimeUnit.MILLISECONDS.toSeconds(milliseconds) -
+        //                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milliseconds)));
+
+        // return timer string
+        return finalTimerString
+    }
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
